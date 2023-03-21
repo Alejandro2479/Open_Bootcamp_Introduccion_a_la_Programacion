d3ba0f6a19d183e7e50a647173bdb1a9bfe29340
Alejandro Otalvaro <65875344+Alejandro2479@users.noreply.github.com>
2023-03-21 15:00:08 -0500

    Create Ejercicios_Tema_4.py

---
+++ Ejercicios_Tema_4.py
@@ -0,0 +1,41 @@
+# Usando un if
+numeroIf = 5
+
+if numeroIf > 0:
+    print("El número es positivo")
+elif numeroIf < 0:
+    print("El número es negativo")
+else:
+    print("El número es cero")
+
+# Bucle While
+numeroWhile = 0
+while numeroWhile < 3:
+    numeroWhile += 1
+    print(numeroWhile)
+
+# Bucle Do While
+numeroDoWhile = 0
+while True:
+    numeroDoWhile += 1
+    print(numeroDoWhile)
+    if numeroDoWhile >= 3:
+        break
+
+# Bucle For
+for numeroFor in range(4):
+    print(numeroFor)
+
+# Switch
+estacion = "verano"
+
+if estacion == "primavera":
+    print("Estamos en primavera")
+elif estacion == "verano":
+    print("Estamos en verano")
+elif estacion == "otoño":
+    print("Estamos en otoño")
+elif estacion == "invierno":
+    print("Estamos en invierno")
+else:
+    print("La variable estacion no tiene un valor válido")
